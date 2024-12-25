e294c967cf6508fcd2feb1d611a04770814215b9
Mahdi <bmd579@gmail.com>
2024-12-26 00:10:19 +0100

    basics

---
+++ src/main/java/se/nordnet/authentication/dev_authenticator/DevAuthenticatorApplication.java
@@ -0,0 +1,40 @@
+package se.nordnet.authentication.dev_authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@SpringBootApplication
+public class DevAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAuthenticatorApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity openSimulatorWithAuthzCode(@RequestParam String code) {
+		return ResponseEntity.ok("code: " + code);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void start() {
+		String url = "https://login.microsoftonline.com/eae4202d-7ff0-44f1-b130-3afd5da7b347/oauth2/v2.0/authorize?redirect_uri=http%3A%2F%2Flocalhost%3A9070&client_id=fe88cb91-1d7f-4d8e-a4e7-b2287bce567b&response_type=code&nonce=ZTlhN2I4OWQtMWEyYi00ZjQ0LTg3YWMtNjk4Yzg5YmM3ZTEx&scope=api://fe88cb91-1d7f-4d8e-a4e7-b2287bce567b/Read%20openid&state=MICROSOFT_ENTRAID_DEV_AUTHENTICATOR"
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			desktop.browse(new URI(url));
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/se/nordnet/authentication/dev_authenticator/DevAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nordnet.authentication.dev_authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
