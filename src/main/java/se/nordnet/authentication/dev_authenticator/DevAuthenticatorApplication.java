b14362822797b4ebcee9a8c3e2ab6cc85f3ba1bc
Mahdi <bmd579@gmail.com>
2024-12-26 00:16:06 +0100

    basics

--- src/main/java/se/nordnet/authentication/dev_authenticator/DevAuthenticatorApplication.java
+++ src/main/java/se/nordnet/authentication/dev_authenticator/DevAuthenticatorApplication.java
@@ -2,7 +2,9 @@ package se.nordnet.authentication.dev_authenticator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +21,10 @@ import java.net.URISyntaxException;
 public class DevAuthenticatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevAuthenticatorApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(DevAuthenticatorApplication.class);
+		builder.headless(false);
+		builder.bannerMode(org.springframework.boot.Banner.Mode.OFF);
+		ConfigurableApplicationContext context = builder.run(args);
 	}
 
 	@GetMapping
@@ -29,7 +34,7 @@ public class DevAuthenticatorApplication {
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void start() {
-		String url = "https://login.microsoftonline.com/eae4202d-7ff0-44f1-b130-3afd5da7b347/oauth2/v2.0/authorize?redirect_uri=http%3A%2F%2Flocalhost%3A9070&client_id=fe88cb91-1d7f-4d8e-a4e7-b2287bce567b&response_type=code&nonce=ZTlhN2I4OWQtMWEyYi00ZjQ0LTg3YWMtNjk4Yzg5YmM3ZTEx&scope=api://fe88cb91-1d7f-4d8e-a4e7-b2287bce567b/Read%20openid&state=MICROSOFT_ENTRAID_DEV_AUTHENTICATOR"
+		String url = "https://login.microsoftonline.com/eae4202d-7ff0-44f1-b130-3afd5da7b347/oauth2/v2.0/authorize?redirect_uri=http%3A%2F%2Flocalhost%3A9070&client_id=fe88cb91-1d7f-4d8e-a4e7-b2287bce567b&response_type=code&nonce=ZTlhN2I4OWQtMWEyYi00ZjQ0LTg3YWMtNjk4Yzg5YmM3ZTEx&scope=api://fe88cb91-1d7f-4d8e-a4e7-b2287bce567b/Read%20openid&state=MICROSOFT_ENTRAID_DEV_AUTHENTICATOR";
 		Desktop desktop = Desktop.getDesktop();
 		try {
 			desktop.browse(new URI(url));
