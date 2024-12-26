14a08e27a92241b965a10b4b8fa09720a56f0ce5
Mahdi <bmd579@gmail.com>
2024-12-26 15:58:38 +0100

    test accounts in properties

--- src/main/java/se/nordnet/authentication/DevAuthenticatorApplication.java
+++ src/main/java/se/nordnet/authentication/DevAuthenticatorApplication.java
@@ -6,17 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.event.EventListener;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.util.UriUtils;
 
+import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Base64;
 
 @SpringBootApplication
@@ -33,27 +28,23 @@ public class DevAuthenticatorApplication {
     @Autowired
     private UserAccountsProperties userAccountsProperties;
 
+    JFrame frame = new JFrame("Dev Authenticator");
+    JButton button = new JButton("login in simulator as robson");
+    Desktop desktop = Desktop.getDesktop();
+
     @EventListener(ApplicationReadyEvent.class)
     public void start() {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            javax.swing.JFrame frame = new javax.swing.JFrame("Dev Authenticator");
-            javax.swing.JButton button = new javax.swing.JButton("login in simulator as robson");
-
-            button.addActionListener(e -> {
-                Desktop desktop = Desktop.getDesktop()  ;
-                try {
-                    URI url = getAuthorizationUrl(userAccountsProperties.userAccounts().get(0).customerId());
-                    desktop.browse(url);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            });
-
-            frame.getContentPane().add(button);
-            frame.setSize(1024, 800);
-            frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
+        button.addActionListener(e -> {
+            try {
+                desktop.browse(getAuthorizationUrl(userAccountsProperties.userAccounts().get(0).customerId()));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
+        frame.getContentPane().add(button);
+        frame.setSize(1024, 800);
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     private URI getAuthorizationUrl(String customerId) {
