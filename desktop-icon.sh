ae3c2cd07943a72a2e17c82f0b7e9c0b3944bbf3
Mahdi <bmd579@gmail.com>
2024-12-31 20:51:04 +0100

    shs

---
+++ desktop-icon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Creating desktop shortcut from $(pwd)."
+
+SHORTCUT_NAME="Nordnet-dev-authenticator"
+#SHORTCUT_PATH="$(pwd)/$SHORTCUT_NAME.command"
+SHORTCUT_PATH="$HOME/Desktop/$SHORTCUT_NAME.command"
+ICON_PATH="$(pwd)/src/main/resources/img.png"
+RUN_SCRIPT="sh $(pwd)/gradlew bootRun"
+
+
+cat <<EOF > "$SHORTCUT_PATH"
+#!/bin/bash
+$RUN_SCRIPT
+EOF
+
+chmod a+x "SHORTCUT_PATH"
+chmod 755 "SHORTCUT_PATH"
+
+echo "Shortcut created in $SHORTCUT_PATH."
