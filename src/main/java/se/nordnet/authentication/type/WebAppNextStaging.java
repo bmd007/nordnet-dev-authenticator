0e22ca050689556e2d365e48730c2417ce27b2e6
Mahdi <bmd579@gmail.com>
2025-01-13 10:56:48 +0100

    webapp next staging

--- src/main/java/se/nordnet/authentication/type/WebAppNextStaging.java
+++ src/main/java/se/nordnet/authentication/type/WebAppNextStaging.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public record WebAppNextStaging(String pullRequestIdentifier) {
     @JsonIgnore
     public String url(String countryCode, String authorizationCode) {
-        return "https://%s.webapp-next.staging.nordnet-test.%s/login?code=%s&state=MICROSOFT_ENTRAID_CARISMA".formatted(pullRequestIdentifier, countryCode, authorizationCode);
+        return "https://%s.webapp-next.staging.nordnet-test.%s/login?code=%s&state=NORDNET_DEV_AUTHENTICATOR_STAGING".formatted(pullRequestIdentifier, countryCode, authorizationCode);
     }
 }
