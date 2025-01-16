a3b868b44885fe60c2da7f6ec79ad24dc4d3d11e
Mahdi <bmd579@gmail.com>
2025-01-16 14:49:22 +0100

    login to stage

--- src/main/java/se/nordnet/authentication/type/WebAppNextStaging.java
+++ src/main/java/se/nordnet/authentication/type/WebAppNextStaging.java
@@ -1,10 +1,17 @@
 package se.nordnet.authentication.type;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public record WebAppNextStaging(String pullRequestIdentifier) {
     @JsonIgnore
     public String url(String countryCode, String authorizationCode) {
-        return "https://%s.webapp-next.staging.nordnet-test.%s/login?code=%s&state=NORDNET_DEV_AUTHENTICATOR_STAGING".formatted(pullRequestIdentifier, countryCode, authorizationCode);
+        return UriComponentsBuilder.fromUriString("https://%s.webapp-next.staging.nordnet-test.%s".formatted(pullRequestIdentifier, countryCode))
+                .path("/login")
+                .queryParam("code", authorizationCode)
+                .queryParam("state", "MICROSOFT_ENTRAID_CARISMA")
+                .queryParam("loginFeatures", "nordet-dev-authenticator-webapp-next-staging-login")
+                .build()
+                .toUriString();
     }
 }
